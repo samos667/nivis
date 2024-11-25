0457a3ce20fe2a3d09fdbfc20e2ac56df0c421f9
Sammy Etur <sammyetur11@gmail.com>
2024-11-25 16:28:00 +0100

    fix: removeee unnecessary linter

--- config/plugins/languages/py.nix
+++ config/plugins/languages/py.nix
@@ -6,7 +6,6 @@
     python312Packages.debugpy
     python312Packages.flake8
   ];
-
   plugins = {
     dap.extensions.dap-python.enable = true;
     lsp.servers.pyright.enable = true;
@@ -15,15 +14,6 @@
       formattersByFt = {
         python = [
           "black"
-        ];
-      };
-    };
-
-    lint = {
-      lintersByFt = {
-        python = [
-          "flake8"
-          "pylint"
         ];
       };
     };
